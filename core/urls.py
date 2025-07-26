0e3b6e2acddbbbd6172fa37adfbb0f83124b1541
Kasilingam M <138505237+lingaa005@users.noreply.github.com>
2025-07-27 01:11:56 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (VendorGroupChatListCreateAPIView,VendorProducerListAPIView,ProducerDetailAPIView
 ,ProducerOwnReviewsAPIView,SubmitRatingView
 )
-from .views import SignupView, LoginView, LogoutView, ChatMessageCreateView, ChatMessageListView,SentMessagesView,AllUserMessagesView
+from .views import LoginView, LogoutView, ChatMessageCreateView, ChatMessageListView,SentMessagesView,AllUserMessagesView,VendorSignupView, ProducerSignupView
 
 urlpatterns = [
     path('vendor-chat/', VendorGroupChatListCreateAPIView.as_view(), name='vendor-chat'),
@@ -10,14 +10,14 @@ urlpatterns = [
     path('producers/<int:producer_id>/', ProducerDetailAPIView.as_view(), name='producer-detail'),
     path('producers/reviews/', ProducerOwnReviewsAPIView.as_view(), name='producer-own-reviews'),
     path('rate-producers/', SubmitRatingView.as_view(), name='vendor-rate-producer'),
-    path('signup/vendor/', VendorSignupView.as_view(), name='vendor-signup'),
-    path('signup/producer/', ProducerSignupView.as_view(), name='producer-signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('messages/', ChatMessageCreateView.as_view(), name='send-message'),
     path('messages/<int:user_id>/', ChatMessageListView.as_view(), name='get-conversation'),
     path('messages/sent/', SentMessagesView.as_view(), name='sent-messages'),
     path('all/', AllUserMessagesView.as_view(), name='all-messages'),
+    path('signup/vendor/', VendorSignupView.as_view(), name='vendor-signup'),
+    path('signup/producer/', ProducerSignupView.as_view(), name='producer-signup'),
 
 
 ]
